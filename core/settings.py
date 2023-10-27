7bf8ae5bb6a3990e058bf1293beda67f1cd8569b
kenzi26 <mokwenyekene1@yahoo.com>
2023-10-27 21:12:47 +0100

    Feat: removed database path

--- core/settings.py
+++ core/settings.py
@@ -204,18 +204,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-#database_url = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse('postgres://keep_am_here_user:vXkPFacXcUG74lA9x7oiqk5RGNjjCOls@dpg-cjfse5hitvpc73dgi5h0-a.oregon-postgres.render.com/keep_am_here')
 
-#for render online database
-#postgres://keep_am_here_user:vXkPFacXcUG74lA9x7oiqk5RGNjjCOls@dpg-cjfse5hitvpc73dgi5h0-a/keep_am_here
-#DEFAULT_API_URL = os.getenv('DEFAULT_API_URL')
-
-#for local host
-#postgres://keep_am_here_user:vXkPFacXcUG74lA9x7oiqk5RGNjjCOls@dpg-cjfse5hitvpc73dgi5h0-a.oregon-postgres.render.com/keep_am_here
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
